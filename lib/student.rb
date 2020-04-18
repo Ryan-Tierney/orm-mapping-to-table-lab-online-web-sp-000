df37a9ba0e3ff3c44bc397b29b66a4dd7dc8f147
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-18 04:21:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,16 @@
 class Student
 
  attr_accessor :name, :grade 
+ attr_reader :id
  
+ def initialize(id = nil, name, grade)
+   @id = id 
+   @name = name 
+   @grade = grade 
+ end 
+ 
+ def self.create_table 
+ end 
  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
